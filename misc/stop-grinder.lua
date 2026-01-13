6dec3fca21ca400979ea8f3f3a393e76101c9acf
TheMrCake <antonlichota@gmail.com>
2026-01-12 23:53:15 -0400

    Fix backwards output on the grinder

--- misc/stop-grinder.lua
+++ misc/stop-grinder.lua
@@ -3,7 +3,10 @@ local meatTank = peripheral.wrap("advancedFluidTank_1") or error("MeatTank not f
 local pinkSlimeTank = peripheral.wrap("advancedFluidTank_0") or error("PinkSlimeTank not found")
 
 while true do
+  local output = true
   if meatTank.getCapacity() == meatTank.getStored().amount then
-    redstone.setOutput("bottom", true)
+    output = false
   end
+
+  redstone.setOutput("bottom", output)
 end
